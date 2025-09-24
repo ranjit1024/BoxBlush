820f1784a597d1dabdd68f77fbe3039d4797ff08
ranjit <ranjitdas2048@gmail.com>
2025-09-24 14:55:02 +0530

    backend added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello, TypeScript!");
+import http from "http";
+import { WebSocketServer } from "ws";
+//
+const httpServer = http.createServer();
+const wss = new WebSocketServer({
+    server:httpServer
+})
+httpServer.listen(8080, ()=>{
+    console.log('isting on pot nmber 8080')
+})
